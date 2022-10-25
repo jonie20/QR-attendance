de9500c4145c920984da093b3b4c747bb86402a6
jonie20 <84018132+jonie20@users.noreply.github.com>
2022-10-25 13:15:04 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\JonieKE\\Downloads\\flutter_windows_2.10.4-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\JonieKE\\flutterdev\\attendanceqr" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\JonieKE\\Downloads\\flutter_windows_2.10.4-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\JonieKE\\flutterdev\\attendanceqr"
+  "FLUTTER_ROOT=C:\\Users\\JonieKE\\Downloads\\flutter_windows_2.10.4-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\JonieKE\\flutterdev\\attendanceqr\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\JonieKE\\flutterdev\\attendanceqr"
+  "FLUTTER_TARGET=C:\\Users\\JonieKE\\flutterdev\\attendanceqr\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\JonieKE\\flutterdev\\attendanceqr\\.dart_tool\\package_config.json"
+)
